f2b26b10f82699b10876b5e563ef5622253b5315
Jack <jgb1017@gmail.com>
2025-09-24 12:01:43 -0500

    Move most rendering logic into a new Renderer class

--- src/Game.h
+++ src/Game.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <array>
+#include <string>
 
 class GameBoard {
     // The Tic Tac Toe board is composed of 9 cells
@@ -14,10 +17,10 @@ class GameBoard {
         void draw_board();
 
         // Draw a circle centered in specific a cell
-        void place_circle(int cellIndex);
+        void place_circle(const int cellIndex);
 
         // Draw an X centered in specific a cell
-        void place_x(int cellIndex);
+        void place_x(const int cellIndex);
 
         // Debug print the grid to output
         void print_grid();
@@ -34,4 +37,31 @@ class GameBoard {
 
         // Helper function to draw one of the rectangle lines on the board 
         void draw_rectangle();
+};
+
+class Renderer {
+    // Abstract much of the OpenGL setup and rendering calls
+    public:
+        // Setup and initialize the OpenGL context
+        Renderer();
+
+        // Render once. This should be called every update
+        void render();
+
+        // Called if a resize window event occurs
+        void resize(const int width, const int height);
+
+        // Load a shader and return an empty string on failure
+        std::string loadShader(const std::string filename);
+
+        bool initFailed() {return initFailure;}
+
+        // Clean up and shut down OpenGL and its context
+        ~Renderer();
+    
+    private:
+        // If construction fails, will be set to true
+        int shaderProgramObject = 0;
+        int vertexArrayObject = 0;
+        bool initFailure = false;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,8 @@
 #include "glad/glad.h"
 #include <SFML/Window.hpp>
-#include <SFML/OpenGL.hpp>
-
-#include <exception>
-#include <filesystem>
-#include <iostream>
-#include <fstream>
-#include <iterator>
-#include <string>
 
 #include "Game.h"
 #include "constants.h"
-
-std::string loadShaders(const std::string filename) {
-    std::ifstream file;
-    file.exceptions(file.exceptions() | std::ios::failbit);
-    try {
-        file.open(filename.c_str());
-    } catch (std::exception e) {
-        std::cout << "Error opening " << filename  << " -> " << e.what() << std::endl;
-        return "";
-    }
-
-    std::string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    file.close();
-    return text;
-}
 
 int main() {
     //*********************************************************
@@ -55,115 +32,12 @@ int main() {
         return -1;
     }
 
-    //*********************************************************
-    // Load shaders from their corresponding files
-    //*********************************************************
-
-    // Load Shaders
-    std::cout << "Working from " << std::filesystem::current_path() << std::endl;
-    std::cout << "Shader path is " << SHADER_PATH << std::endl;
-    std::string vertexShaderString = loadShaders(TTT::vertexShaderPath);
-    std::string fragmentShaderString = loadShaders(TTT::fragmentShaderPath);
-    if (vertexShaderString == "" || fragmentShaderString == "") {
+    // Setup renderer
+    Renderer glRenderer = Renderer();
+    if (glRenderer.initFailed()) {
         return -1;
     }
-
-    //*********************************************************
-    // Prepare OpenGL and its shader pipeline
-    //*********************************************************
-
-    // GL Setup
-    glClearColor(0.1f, 0.0f, 0.1f, 1.0f);
-
-    // See the LearnOpenGL textbook
-    // Store the rectangle's vertices
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // bottom left
-        0.5f, -0.5f, 0.0f, // bottom right
-        0.5f, 0.5f, 0.0f, // top right
-        -0.5f, 0.5f, 0.0f // top left
-    };
-
-    // Store which vertices correspond to which shape
-    unsigned int indices[] = {
-        0, 1, 3, // triangle 1
-        1, 2, 3 // triangle 2
-    };
-
-    // Create a vertex array object (VAO) to store vertex attribute states
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    // Create a vertex buffer object to store the vertex data
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // Store which indices OpenGL should use to draw
-    unsigned int EBO;
-    glGenBuffers(1, &EBO); 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    // Compile vertex shader
-    const char* vertexShaderSource = vertexShaderString.c_str();
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    // Check if vertex shader compilation was successful
-    int vertexShaderCompilationSuccess;
-    char vertexInfoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderCompilationSuccess);
-    if (!vertexShaderCompilationSuccess) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, vertexInfoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << vertexInfoLog << std::endl;
-    }
-
-    // Compile fragment shader
-    const char* fragmentShaderSource = fragmentShaderString.c_str();
-    unsigned int fragmentShader;
-    fragmentShader= glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // Check if fragment shader compilation was successful
-    int fragmentShaderCompilationSuccess;
-    char fragmentInfoLog[512];
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentShaderCompilationSuccess);
-    if (!fragmentShaderCompilationSuccess) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, fragmentInfoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << fragmentInfoLog << std::endl;
-    }
-
-    // Setup shader program
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Check for shader program success
-    int shaderProgramSuccess;
-    char programInfoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgramSuccess);
-    if (!shaderProgramSuccess) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, programInfoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << programInfoLog << std::endl;
-    }
-
-    // Delete the now unneeded (after linking) shader objects
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Link the vertex attributes
-    // Note that the previous VBO is still bound, so this will apply to that
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
+    
     //*********************************************************
     // Begin the main game loop
     //*********************************************************
@@ -178,21 +52,12 @@ int main() {
             } 
             else if (const auto* resized = event->getIf<sf::Event::Resized>())
             {
-                glViewport(0, 0, resized->size.x, resized->size.y);
+                glRenderer.resize(resized->size.x, resized->size.y);
             }
         }
 
-        // Clear buffers
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Prepare to draw
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO); // Remembers which buffers are bound already automatically
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
-        // Draw
-        static int xPos = 0;
+        // Render a new frame to the screen
+        glRenderer.render();
 
         // End the frame (internally swaps front and back buffers)
         window.display();
